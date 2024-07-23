1345f9a8ceb5a720c343b0efa12b2580e8226a4f
jahinzee <jahinzee@outlook.com>
2024-07-23 14:23:37 +1000

    + added rss feed + build: resolved astro hints + chore: improved code formatting

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,50 @@
+import rss from '@astrojs/rss';
+
+async function unravelGlob(glob) {
+    let output = []
+    for (const promise of Object.values(glob)) {
+        promise().then((p) => output.push(p))
+    };
+    return output;
+}
+
+const photos = await unravelGlob(import.meta.glob('./photos/*.mdx'));
+const articles = await unravelGlob(import.meta.glob('./articles/*.mdx'));
+const graphics = await unravelGlob(import.meta.glob('./graphics/*.mdx'));
+
+export function GET(context) {
+
+    return rss({
+        title: 'jahinzee',
+        description: 'jahinzee\'s photos, articles, and graphics.',
+        site: context.site,
+        items: []
+            .concat(photos.map((p) => {
+                return {
+                    title: `Photo: ${p.frontmatter.title}`,
+                    description: p.frontmatter.location !== undefined ? `Taken at ${p.frontmatter.location}.` : "",
+                    pubDate: p.frontmatter.date,
+                    link: p.url,
+                    categories: ["photos"]
+                }
+            }))
+            .concat(articles.map((a) => {
+                return {
+                    title: `Article: ${a.frontmatter.title}`,
+                    description: "",
+                    pubDate: a.frontmatter.date,
+                    link: a.url,
+                    categories: ["articles"]
+                }
+            }))
+            .concat(graphics.map((g) => {
+                return {
+                    title: `Graphic: ${g.frontmatter.title}`,
+                    description: "",
+                    pubDate: g.frontmatter.date,
+                    link: g.url,
+                    categories: ["graphics"]
+                }
+            }))
+    });
+}
