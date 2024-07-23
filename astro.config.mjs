6309da8a30db1f821f784daf544b9fb12e2b76ff
jahinzee <jahinzee@outlook.com>
2024-07-23 20:33:53 +1000

    content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
